5f0b5694ecc359d7dc3b34b4a42c15176647c3d8
albayu <ubaidilahalbayu.com>
2023-05-05 13:36:43 +0700

    first commit

---
+++ coba.js
@@ -0,0 +1,8 @@
+const a = 2
+const b = "yes"
+const c = true
+function name(params) {
+    var z = a
+    var x = b
+    var s = c
+}
---
+++ subfolder/coba.php
@@ -0,0 +1,10 @@
+<?php
+
+$x = 1;
+$y = 2;
+$c = "<b>YA</b>";
+function FunctionName($var = null)
+{
+    $a = true;
+    $b = "yes";
+}
